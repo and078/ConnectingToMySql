e5b078f346ca9af42a890920b0f08689f0faceac
and078 <34898237+and078@users.noreply.github.com>
2021-12-16 00:17:20 +0200

    Add project files.

---
+++ BlazorServer/Models/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Models
+{
+    public class PersonModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public int flag { get; set; }
+    }
+}
