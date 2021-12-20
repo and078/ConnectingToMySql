10fc7012c7180d7c955cad5cd5ce8f5fd9c7bb6f
and078 <34898237+and078@users.noreply.github.com>
2021-12-20 22:49:35 +0200

    add some

---
+++ BlazorServer/Models/ProductModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorServer.Models
+{
+    public class ProductModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int price { get; set; }
+    }
+}
